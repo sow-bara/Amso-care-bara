f92f352e195d0fa9267c8ddfb1ce70c13861104a
apple <apple@macbookpro.home>
2025-02-03 11:21:21 +0000

    Mis a jour de AmsoCare

--- app/WelcomeImage.tsx
+++ app/WelcomeImage.tsx
@@ -1,7 +1,14 @@
 import { Link } from "expo-router";
 import { ArrowRight } from "lucide-react-native";
 import React from "react";
-import { View, Image, StyleSheet, Text, Dimensions } from "react-native";
+import {
+  View,
+  Image,
+  StyleSheet,
+  Text,
+  Dimensions,
+  SafeAreaView,
+} from "react-native";
 
 const { width, height } = Dimensions.get("window");
 
@@ -13,30 +20,30 @@ type Props = {
 
 const WelcomeImage = ({ imageSource, steps, currentStep }: Props) => {
   return (
-    <View>
-      <View style={styles.imageContainer}>
-        <Image source={imageSource} style={styles.image} />
+    <SafeAreaView style={styles.safeArea}>
+        <View style={styles.imageContainer}>
+          <Image source={imageSource} style={styles.image} />
+        </View>
+        <View style={styles.containerLogo}>
+          <Image source={require("@/assets/images/logo-amso.png")} style={styles.logo} />
+        </View>
+        <View style={styles.containerPasser}>
+          <Text style={styles.text}>Passer</Text>
+          <ArrowRight size={24} color="white" />
+        </View>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Votre véhicule entre de bonnes mains</Text>
+        </View>
+        <View style={styles.containerMessage}>
+          <Text style={styles.textMessage}>
+            Prenez soin de votre véhicule sans effort. Du diagnostic à la vidange, on s’occupe de tout pour que vous rouliez l’esprit tranquille.
+          </Text>
+        </View>
+        <View style={styles.button}>
+        <Link href="/Booking">
+          <ArrowRight size={24} color="#24294F" />
+        </Link>
       </View>
-      <View style={styles.containerLogo}>
-        <Image source={require("@/assets/images/logo-amso.png")} style={styles.logo} />
-      </View>
-      <View style={styles.containerPasser}>
-        <Text style={styles.text}>Passer</Text>
-        <ArrowRight size={24} color="white" />
-      </View>
-      <View style={styles.titletext}>
-        <Text style={styles.title}>Votre véhicule entre de bonnes mains</Text>
-      </View>
-      <View style={styles.containerMessage}>
-        <Text style={styles.textMessage}>
-          Prenez soin de votre véhicule sans effort. Du diagnostic à la vidange, on s’occupe de tout pour que vous rouliez l’esprit tranquille.
-        </Text>
-      </View>
-      <View style={styles.button}>
-        <Link href="./Booking"> <ArrowRight size={24} color="#24294F" /> </Link>  
-      </View>
-      
-      {/* Stepper intégré */}
       <View style={styles.stepperContainer}>
         {Array.from({ length: steps }).map((_, index) => (
           <View
@@ -48,25 +55,29 @@ const WelcomeImage = ({ imageSource, steps, currentStep }: Props) => {
           />
         ))}
       </View>
-    </View>
+      
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  safeArea: {
+    top: "5%"
+  },
+  
   imageContainer: {
-    alignItems: "center",
     justifyContent: "center",
-    marginTop: height * 0.06,
+    alignItems: "center",
   },
   image: {
-    width: width * 0.92,
-    height: height * 0.5,
+    width: width * 0.9,
+    height: height * 0.45,
     borderRadius: 24,
     opacity: 0.6,
   },
   containerLogo: {
     position: "absolute",
-    top: height * 0.07,
+    top: height * 0.01,
     alignSelf: "center",
   },
   logo: {
@@ -77,56 +88,56 @@ const styles = StyleSheet.create({
   containerPasser: {
     flexDirection: "row",
     position: "absolute",
-    top: height * 0.07,
+    top: height * 0.01,
     right: width * 0.06,
     alignItems: "center",
+    cursor: "pointer",
   },
   text: {
     fontSize: 20,
     color: "white",
     marginRight: 8,
   },
-  titletext: {
+  titleContainer: {
     width: "86%",
     alignSelf: "center",
     marginTop: height * 0.02,
   },
   title: {
     color: "white",
-    fontSize: width * 0.08,
+    fontSize: 30,
     fontWeight: "bold",
     textAlign: "left",
   },
   containerMessage: {
     width: "85%",
-    top: height * 0.01,
+    marginTop: height * 0.02,
+    
   },
   textMessage: {
     color: "white",
-    fontSize: width * 0.048,
+    fontSize: 25,
     textAlign: "justify",
-    left: width * 0.06,
-  },
+    left: width * 0.08,
+  }
+  ,
   button: {
     backgroundColor: "white",
     right: width * 0.05,
-    bottom: height * 0.05,
-    position: "absolute",
-    width: width * 0.18,
-    height: width * 0.18,
-    borderRadius: width * 0.07,
+    width: 60,
+    height: 60,
+    borderRadius: 19,
     justifyContent: "center",
     alignItems: "center",
-    top: height * 0.8,
+    position: "absolute",
+    top: height * 0.79,
   },
   stepperContainer: {
     flexDirection: "row",
-    marginVertical: 45,
-    left: width * 0.05,
+    left: width * 0.07,
     position: "absolute",
     alignSelf: "flex-start",
-    top: height * 0.8,
-
+    top: height * 0.83,
   },
   step: {
     width: 8,
@@ -140,6 +151,7 @@ const styles = StyleSheet.create({
   },
   inactiveStep: {
     backgroundColor: "#ffffff",
+    opacity: 0.5,
   },
 });
 
