2233db642ce2300733c395fe23d53552dfd812bd
apple <apple@macbookpro.home>
2025-02-04 14:15:37 +0000

    Mis a jour de AmsoCare

--- components/HeaderBooking.tsx
+++ components/HeaderBooking.tsx
@@ -1,31 +1,38 @@
-import { useRouter } from "expo-router";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import React from "react";
+import { View, Text, TouchableOpacity, StyleSheet, SafeAreaView } from "react-native";
 import { Bell, ChevronLeft } from "lucide-react-native";
+import { StatusBar } from "expo-status-bar";
+import { router } from "expo-router";
 
 const HeaderBooking = () => {
-  const router = useRouter();
-
   return (
-    <View style={styles.container}>
-      <TouchableOpacity onPress={() => router.back()} style={styles.iconContainer}>
-        <ChevronLeft size={20} color="#24294F" />
-      </TouchableOpacity>
-      <Text style={styles.text}>Prise de rendez-vous</Text>
-      <View style={styles.iconContainer}>
-        <Bell size={20} color="#24294F" />
+    <SafeAreaView style={styles.safeArea}>
+      <StatusBar style="dark" />
+      <View style={styles.container}>
+        <TouchableOpacity onPress={() => router.back()} style={styles.iconContainer}>
+          <ChevronLeft size={20} color="#24294F" />
+        </TouchableOpacity>
+        <Text style={styles.text}>Prise de rendez-vous</Text>
+        <View style={styles.iconContainer}>
+          <Bell size={20} color="#24294F" />
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 0,
+    backgroundColor: "#FFFFFF", // Assure que l'arrière-plan est le même que l'en-tête
+    paddingTop: 50, // Ajuste cette valeur pour compenser la barre d'état sur Android
+  },
   container: {
     flexDirection: "row",
     alignItems: "center",
     paddingHorizontal: 20,
     justifyContent: "space-between",
-    backgroundColor: "#FFFFFF", // Ajout du fond blanc
-    // height: 80,
+    backgroundColor: "#FFFFFF", // Même arrière-plan que SafeAreaView
   },
   text: {
     fontSize: 18,
